e6240c898a3daedb6c71609609996d7d7d833e2a
Nayem55 <nayem.sayed.web@gmail.com>
2024-05-16 00:10:02 +0600

    added post api for products

--- models/products.model.js
+++ models/products.model.js
@@ -2,12 +2,25 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    _id: { type: String, required: true },
+    _id: { type: String },
     Brand: { type: String, required: true },
     name: { type: String, required: true},
+    slug: { type: String, required: true},
+    sku: { type: String},
+    description: { type: String},
+    meta_description: { type: String},
+    short_description: { type: String},
+    price: { type: String},
+    sale_price: { type: String},
+    on_sale: { type: String},
+    short_description: { type: String},
     category: { type: String, required: true },
     subcategory: { type: String, required: true },
     reviews: { type: Array },
+    attributes: { type: Array },
+    tags: { type: Array },
+    images: { type: Array },
+    variations: { type: Array },
   },
   { strict: false }
 );
