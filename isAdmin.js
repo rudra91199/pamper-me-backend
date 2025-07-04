9e9e2346a20548996c7e75e86aa2515f610269a2
rudra91199 <92229266+rudra91199@users.noreply.github.com>
2025-07-05 00:42:56 +0600

    admin

---
+++ isAdmin.js
@@ -0,0 +1,13 @@
+import { DashboardUser } from "../modules/dashBoardUser/dashboardUser.model.js";
+
+const isAdmin = async (req, res, next) => {
+  const requester = req?.userId;
+  const requesterAccount = await DashboardUser.findOne({ _id: requester });
+  if (requesterAccount?.role !== "admin") {
+    return res.status(403).send({ message: "Forbidden Access." });
+  } else {
+    next();
+  }
+};
+
+export default isAdmin;
